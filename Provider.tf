e8e0fa82b2613f1dd6557f63ba75498263d580a6
sejalofficial798 <sejalofficial798@.com>
2025-01-04 22:00:36 +0530

    cred-add

--- Provider.tf
+++ Provider.tf
@@ -10,5 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  access_key = "AKIAVRUVVLW4YYK6U53R"
+  secret_key = "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"
   
 }
